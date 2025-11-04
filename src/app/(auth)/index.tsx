56fc33c408cd501ba00455705654416116d4f7ab
Uirá Cardoso <88112570+U1R4@users.noreply.github.com>
2025-11-04 17:31:12 -0300

    O pai e brabo

---
+++ src/app/(auth)/index.tsx
@@ -0,0 +1,9 @@
+import AuthContainer from "@/components/ui/AuthContainer";
+
+export default function Login(){
+    return <AuthContainer 
+    title="Bem-Vindo"
+    subtitle="Faça seu login"
+    icon="hotel">
+    </AuthContainer>
+}
--- src/components/ui/AuthContainer.tsx
+++ src/components/ui/AuthContainer.tsx
@@ -1,14 +1,22 @@
-import {Ionicons} from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 type Props = {
     title: string;
     subtitle?:string;
-    icon?: keyof typeof Ionicons.glyphMap;
+    icon?: keyof typeof FontAwesome5.glyphMap;
+    // children: React.ReactNode;
 }
 
-export default function AuthContainer(){
+export default function AuthContainer({title, subtitle, icon, /*children*/}:Props){
 
     return (
-
+        <SafeAreaView>
+            <FontAwesome5 name={icon} size={25} color="black"/>
+            <Text>{title}</Text>
+            <Text>{subtitle}</Text>
+        </SafeAreaView>
     );
 }
