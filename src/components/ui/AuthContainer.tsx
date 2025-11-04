ccb7c3d9ac94e6c5e98087a3e363f76a4a3fbb3a
Uirá Cardoso <88112570+U1R4@users.noreply.github.com>
2025-11-04 16:38:39 -0300

    Hora do Mobile
    Set up a new Expo project including initial directory structure, configuration files, assets, and basic authentication and tab navigation components. Added ESLint configuration, VSCode settings, and project documentation.

---
+++ src/components/ui/AuthContainer.tsx
@@ -0,0 +1,14 @@
+import {Ionicons} from "@expo/vector-icons";
+
+type Props = {
+    title: string;
+    subtitle?:string;
+    icon?: keyof typeof Ionicons.glyphMap;
+}
+
+export default function AuthContainer(){
+
+    return (
+
+    );
+}
